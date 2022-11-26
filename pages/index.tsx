48160f2256c965ea59cfdd6b3abb26331b321a03
juanflarrotta <juanlarrotta@gmail.com>
2022-11-26 12:50:36 -0500

    Se crea la base de datos de los dos primeros pasos, y se envian a redux cuando carga el home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,39 @@
-import React, { ReactElement } from 'react';
-import Background from '@components/background';
+import React, { ReactElement, useEffect } from 'react';
 
-const Home = (): ReactElement => (
-  <>
-    <Background />
-  </>
-);
+import { useDispatch } from 'react-redux';
+import { setSteps } from 'redux/slices/stepsSlice';
+
+import Background from '@components/background';
+import { Steps } from 'types';
+
+type Props = {
+  steps: Steps;
+};
+
+const Home = ({ steps }: Props): ReactElement => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setSteps(steps));
+  }, []);
+
+  return (
+    <>
+      <Background />
+    </>
+  );
+};
 export default Home;
+
+export async function getServerSideProps() {
+  let steps = null;
+  try {
+    const res = await fetch('http://localhost:3000/api/steps');
+    steps = await res.json();
+  } catch (error) {
+    console.log('Steps fetching error');
+    console.log(error);
+  }
+
+  return { props: { steps } };
+}
